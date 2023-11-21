aef63d05985a0f2757bba6eca9f81ae96ab0502e
RDSM243 <didicomendes13@gmail.com>
2023-11-21 14:23:39 -0300

    Background image
    clean view of neovim, removed pane control, change of colorscheme.

--- wezterm.lua
+++ wezterm.lua
@@ -6,36 +6,31 @@ wezterm.on('update-right-status', function(window, pane)
 end)
 
 return {
-  color_scheme = 'Gruvbox dark, medium (base16)',
+  color_scheme = 'tokyonight',
   font = wezterm.font 'Hack NF',
-  window_close_confirmation = "NeverPrompt", 
+  enable_scroll_bar = false,
+  window_padding = {left = 0, right = 0, top = 0, bottom = 0,},
+  window_close_confirmation = "NeverPrompt",
   initial_rows = 34, -- window height
   initial_cols = 88, --window width
   font_size = 11.0,
   hide_tab_bar_if_only_one_tab = true,
+  background = {
+    {
+      source = { File = 'C:/Users/MSDR1/.config/wezterm/background.jpg',},
+      width = '100%',
+      hsb = {brightness = 0.03}, --background brightness
+      opacity = 1,
+    },
+  },
   keys = {
     { key = 'c', mods = 'CTRL', action = wezterm.action.Copy },
     { key = 'v', mods = 'CTRL', action = wezterm.action.Paste },
-    -- This will create a new split at the bottom of the current term
-    {
-      key = 't',
-      mods = 'CTRL',
-      action = wezterm.action.SplitPane {
-        direction = 'Down',
-        size = { Percent = 5 },
-      },
-    },
-    --This will close the current pane you are focusing in
-    {
-      key = 'f',
-      mods = 'CTRL',
-      action = wezterm.action.CloseCurrentPane { confirm = true },
-    },
     --Show a list of possible actions in wezterm
-    { 
-      key = 'a', 
-      mods = 'CTRL', 
-      action = wezterm.action.ShowLauncher 
+    {
+      key = 'a',
+      mods = 'CTRL',
+      action = act.ShowLauncher
     },
     --Opens the workspace menu
     {
@@ -45,5 +40,40 @@ return {
         flags = 'FUZZY|WORKSPACES',
       },
     },
+    -- {
+    --   key = 'LeftArrow',
+    --   mods = 'CTRL',
+    --   action = act.ActivatePaneDirection 'Left',
+    -- },
+    -- {
+    --   key = 'RightArrow',
+    --   mods = 'CTRL',
+    --   action = act.ActivatePaneDirection 'Right',
+    -- },
+    -- {
+    --   key = 'UpArrow',
+    --   mods = 'CTRL',
+    --   action = act.ActivatePaneDirection 'Up',
+    -- },
+    -- {
+    --   key = 'DownArrow',
+    --   mods = 'CTRL',
+    --   action = act.ActivatePaneDirection 'Down',
+    -- },
+    -- -- This will create a new split at the bottom of the current term
+    -- {
+    --   key = 't',
+    --   mods = 'CTRL',
+    --   action = act.SplitPane {
+    --     direction = 'Down',
+    --     size = { Percent = 5 },
+    --   },
+    -- },
+    -- --This will close the current pane you are focusing in
+    -- {
+    --   key = 'f',
+    --   mods = 'CTRL',
+    --   action = act.CloseCurrentPane { confirm = true },
+    -- },
   },
 }
